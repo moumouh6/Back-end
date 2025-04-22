7550d5b4e6db35b404f70f12dba4952714ad2553
Alem Mohamed <126617660+moumouh6@users.noreply.github.com>
2025-04-22 10:05:50 +0100

    Delete app directory

--- app/database.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, create_engine, Session
-
-DATABASE_URL = "sqlite:///./database.db"
-engine = create_engine(DATABASE_URL, echo=True)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
